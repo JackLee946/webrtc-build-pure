82ceb25d110ef33e5b9abe3c3553724e280e85d0
lihuan <745917886@qq.com>
2026-01-19 20:43:36 +0800

    支持windows x86平台

--- run.py
+++ run.py
@@ -169,6 +169,14 @@ PATCH_INFO = {
 }
 
 PATCHES = {
+    'windows_x86': [
+        '4k.patch',
+        'add_license_dav1d.patch',
+        'windows_add_deps.patch',
+        'windows_silence_warnings.patch',
+        'windows_fix_towupper.patch',
+        'ssl_verify_callback_with_native_handle.patch',
+    ],
     'windows_x86_64': [
         '4k.patch',
         'add_license_dav1d.patch',
@@ -413,7 +421,7 @@ def init_rootfs(sysroot: str, config: MultistrapConfig, force=False):
 
 COMMON_GN_ARGS = [
     "rtc_include_tests=false",
-    "rtc_use_h264=false",
+    "rtc_use_h264=true",
     "is_component_build=false",
     'rtc_build_examples=false',
     "use_rtti=true",
@@ -438,7 +446,7 @@ WEBRTC_BUILD_TARGETS = {
 
 def get_build_targets(target):
     ts = [':default']
-    if target not in ('windows_x86_64', 'windows_arm64', 'ios', 'macos_x86_64', 'macos_arm64'):
+    if target not in ('windows_x86', 'windows_x86_64', 'windows_arm64', 'ios', 'macos_x86_64', 'macos_arm64'):
         ts += ['buildtools/third_party/libc++']
     ts += WEBRTC_BUILD_TARGETS.get(target, [])
     return ts
@@ -664,11 +672,15 @@ def build_webrtc(
             f"is_debug={'true' if debug else 'false'}",
             *COMMON_GN_ARGS,
         ]
-        if target in ['windows_x86_64', 'windows_arm64']:
+        if target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
             gn_args += [
                 'target_os="win"',
-                f'target_cpu="{"x64" if target == "windows_x86_64" else "arm64"}"',
+                'target_cpu="x64"',
+                # f'target_cpu="{"x86" if target == "windows_x86" else ("x64" if target == "windows_x86_64" else "arm64")}"',
                 "use_custom_libcxx=false",
+                "use_lld=false",
+                "use_rtti=true",
+                "ffmpeg_branding=\"Chrome\"",
             ]
         elif target in ('macos_x86_64', 'macos_arm64'):
             gn_args += [
@@ -722,7 +734,7 @@ def build_webrtc(
         return
 
     cmd(['ninja', '-C', webrtc_build_dir, *get_build_targets(target)])
-    if target in ['windows_x86_64', 'windows_arm64']:
+    if target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
         pass
     elif target in ('macos_x86_64', 'macos_arm64'):
         ar = '/usr/bin/ar'
@@ -730,7 +742,7 @@ def build_webrtc(
         ar = os.path.join(webrtc_src_dir, 'third_party/llvm-build/Release+Asserts/bin/llvm-ar')
 
     # ar で libwebrtc.a を生成する
-    if target not in ['windows_x86_64', 'windows_arm64']:
+    if target not in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
         archive_objects(ar, os.path.join(webrtc_build_dir, 'obj'), os.path.join(webrtc_build_dir, 'libwebrtc.a'))
 
     # macOS の場合は WebRTC.framework に追加情報を入れる
@@ -761,7 +773,7 @@ def build_webrtc(
 
 
 def copy_headers(webrtc_src_dir, webrtc_package_dir, target):
-    if target in ['windows_x86_64', 'windows_arm64']:
+    if target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
         # robocopy の戻り値は特殊なので、check=False にしてうまくエラーハンドリングする
         # https://docs.microsoft.com/ja-jp/troubleshoot/windows-server/backup-and-storage/return-codes-used-robocopy-utility
         r = cmd(['robocopy', webrtc_src_dir, os.path.join(webrtc_package_dir, 'include'),
@@ -847,7 +859,7 @@ def package_webrtc(source_dir, build_dir, package_dir, target,
     generate_version_info(webrtc_src_dir, webrtc_package_dir)
 
     # ライブラリ
-    if target in ['windows_x86_64', 'windows_arm64']:
+    if target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
         files = [
             (['obj', 'webrtc.lib'], ['lib', 'webrtc.lib']),
         ]
@@ -894,18 +906,19 @@ def package_webrtc(source_dir, build_dir, package_dir, target,
 
     # 圧縮
     with cd(package_dir):
-        if target in ['windows_x86_64', 'windows_arm64']:
-            with zipfile.ZipFile('webrtc.zip', 'w') as f:
+        if target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
+            with zipfile.ZipFile(f'webrtc.{target}.zip', 'w') as f:
                 for file in enum_all_files('webrtc', '.'):
                     f.write(filename=file, arcname=file)
         else:
-            with tarfile.open('webrtc.tar.gz', 'w:gz') as f:
+            with tarfile.open(f'webrtc.{target}.tar.gz', 'w:gz') as f:
                 for file in enum_all_files('webrtc', '.'):
                     f.add(name=file, arcname=file)
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 TARGETS = [
+    'windows_x86',
     'windows_x86_64',
     'windows_arm64',
     'macos_x86_64',
@@ -928,7 +941,7 @@ def check_target(target):
 
     if platform.system() == 'Windows':
         logging.info(f'OS: {platform.system()}')
-        return target in ['windows_x86_64', 'windows_arm64']
+        return target in ['windows_x86', 'windows_x86_64', 'windows_arm64']
     elif platform.system() == 'Darwin':
         logging.info(f'OS: {platform.system()}')
         return target in ('macos_x86_64', 'macos_arm64', 'ios')
@@ -1045,7 +1058,7 @@ def main():
             package_dir = args.package_dir
         webrtc_package_dir = os.path.abspath(args.webrtc_package_dir) if args.webrtc_package_dir is not None else None
 
-    if args.target in ['windows_x86_64', 'windows_arm64']:
+    if args.target in ['windows_x86', 'windows_x86_64', 'windows_arm64']:
         # Windows の WebRTC ビルドに必要な環境変数の設定
         mkdir_p(build_dir)
         download("https://github.com/microsoft/vswhere/releases/download/2.8.4/vswhere.exe", build_dir)
